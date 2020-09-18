1dbc678149700859bfbfde1afd4c3bc386aaf68b
Ishvendra Singh <71009922+Ishvendra@users.noreply.github.com>
2020-09-19 02:05:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+let moles = document.querySelectorAll('.mole');
+let moleArr = Array.prototype.slice.call(moles);
+let random;
+let spot;
+let score = 0;
+let scorefield = document.querySelector('.score');
+let timefield = document.querySelector('.timer');
+let c = timefield.textContent;
+let lastr;
+function gameMain() {
+	moleArr.forEach((curr) => {
+		curr.classList.remove('mole-active');
+	});
+	random = Math.floor(Math.random() * moleArr.length);
+	// console.log(random);
+	if (random === lastr) gameMain();
+	moleArr[random].classList.add('mole-active');
+	spot = moleArr[random].id;
+	// console.log(spot);
+}
+function gamescore() {
+	moleArr.forEach((curr) => {
+		curr.addEventListener('click', function () {
+			if (spot == curr.id) {
+				score++;
+				scorefield.textContent = score;
+			}
+		});
+	});
+}
+gamescore();
+let interval;
+function gametimer() {
+	if (c > 0) {
+		c--;
+		timefield.textContent = c;
+	} else {
+		clearInterval(interval);
+		moleArr[random].classList.remove('mole-active');
+	}
+}
+function gameplay() {
+	gameMain();
+	gametimer();
+}
+let playbtn = document.querySelector('.play');
+playbtn.addEventListener('click', function () {
+	playbtn.classList.add('play-invisible');
+	if (!interval) {
+		interval = setInterval(gameplay, 1000);
+	}
+});
+document.querySelector('.reset').addEventListener('click', function () {
+	location.reload();
+});
+function load(){
+	alert("Happy Birthday Aparajita 🥳")
+	var name1=prompt("Enter Your Name: ");
+	document.getElementById('NAME').innerHTML="Welcome "+name1+"!!";
+}
+var x = document.getElementById("myAudio"); 
+
+function playAudio() { 
+  x.play(); 
+} 
